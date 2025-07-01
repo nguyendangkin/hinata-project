525a44b83ed1a898e528ec294b61cbc47da0aac0
nguyendangkin <nguyenchin0077@gmail.com>
2025-07-01 12:48:33 +0700

    fix flick

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -39,8 +39,8 @@ export default function RootLayout({
                         <ContentLayout>{children}</ContentLayout>
                         <FooterLayout />
                     </AntdProvider>
+                    <NextTopLoader />
                 </AntdRegistry>
-                <NextTopLoader />
             </body>
         </html>
     );
