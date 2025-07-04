443ff9cfdc00bd1ed58835710b6bbe25f58aa1de
nguyendangkin <nguyenchin0077@gmail.com>
2025-07-04 08:42:08 +0700

    up

--- frontend/src/components/page/donate/DonateUi.tsx
+++ frontend/src/components/page/donate/DonateUi.tsx
@@ -50,7 +50,7 @@ export default function DonateUi() {
                         trì hệ thống yêu cầu chi phí cho máy chủ, tên miền và
                         vận hành. Nếu thấy dự án hữu ích, bạn có thể tùy tâm ủng
                         hộ chúng tôi để duy trì và phát triển thêm những tính
-                        năng nữa nhé!
+                        năng mới nữa nhé!
                     </Paragraph>
 
                     <Divider />
